8165106152ce2f6d82c9dda8e47ce7e874c55ed3
xia0ne <1223605525@qq.com>
2023-06-30 15:00:14 +0800

    test: 测试规则

---
+++ check_duplicates.py
@@ -0,0 +1,35 @@
+import os
+from collections import defaultdict
+import hashlib
+
+def calculate_md5(file_path):
+    with open(file_path, 'rb') as f:
+        data = f.read()
+        md5_hash = hashlib.md5(data).hexdigest()
+    return md5_hash
+
+def check_for_duplicates(directory):
+    file_hashes = defaultdict(list)
+    duplicate_files = []
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            md5_hash = calculate_md5(file_path)
+            file_hashes[md5_hash].append(file_path)
+
+    for hash_value, file_paths in file_hashes.items():
+        if len(file_paths) > 1:
+            duplicate_files.extend(file_paths)
+
+    if duplicate_files:
+        print("Error: Duplicate files found!")
+        for file_path in duplicate_files:
+            print(f"\t- {file_path}")
+        exit(1)
+    else:
+        print("No duplicate files found.")
+        exit(0)
+
+directory_path = "."
+check_for_duplicates(directory_path)
