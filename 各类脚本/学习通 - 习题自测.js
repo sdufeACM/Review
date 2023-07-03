3b3f62334c82b7b76a7f97aa48cef080f03f8223
mslxl <i@mslxl.com>
2023-07-03 15:48:54 +0800

    Update and rename 学习通.js to 学习通 - 习题自测.js

--- 各类脚本/学习通 - 习题自测.js
+++ 各类脚本/学习通 - 习题自测.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       mslxl<i@mslxl.com>
 // @match        https://mooc1.chaoxing.com/mooc2/work/view*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chaoxing.com
 // @grant        none
